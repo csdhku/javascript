3c402e28a1fbfe14a655975edd8257e6d71758af
Marc_G <marcg@dinkum.nl>
2017-12-02 22:50:49 +0100

    Explanatory text

--- p5/getting_started/sketch.js
+++ p5/getting_started/sketch.js
@@ -1,3 +1,10 @@
+/*
+ * This sketch is just a little less empty than p5js' empty example
+ *
+ * Opening index.html in a browser should show a white rectangle in the
+ *   upper left corner of a dark red canvas
+ */
+
 
 function setup() {
   createCanvas(640,480);
